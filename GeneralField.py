bf5249a36f7520892c779eb65c54b6a85c3037d4
Vassah <stephen.lazaro@gmail.com>
2014-07-11 09:51:30 -0700

    Add Updated Methods from Numbers.py
    Increase the interaction between the element and field classes. Not sure how good of practice that is, but it seems nice, and I can't think of a better way to do it.

--- GeneralField.py
+++ GeneralField.py
@@ -5,16 +5,41 @@ class GeneralField():
         self.cardinality = len(self.elements)
 
     def add(self, a, b):
-        return (a+b)%self.cardinality
+        return Element(self, (a+b))
 
     def mult(self, a, b):
-        return (a*b)%self.cardinality
+        return Element(self, (a*b))
+        
+    def sub(self, a, b):
+        return Element(self, (a-b))
 
-    def is_square(self, a):
-        return is_Square(a, self.cardinality)
+    def is_square(self, number, is_prime = False):
+        if is_prime:
+            return self.exponent(number, (self.cardinality - 1) // 2, self.cardinality) == 1
+        if Legendre(number, field_size)==1: #This needs replacing since we don't have a Legendre method here
+            return True
+    return False
 
-    def exponent(self, power):
-        return
+    def exponent(self, number, exponent):
+        if exponent == 0:
+            return 1
+        if number or exponent is float:
+            try:
+                number = int(number)
+                exponent = int(exponent)
+            except:
+                print('You\'re number and exponent can\'t be true floats.')
+        cut = bin(exponent).index('b') #Python nicely gives us 0xb but we don't need that information
+        binexp = bin(exponent)[cut+1:][::-1] #Take the binary powers and reverse them
+        exponents = [i for i in range(0, len(binexp)) if int(binexp[i])==1] #make an array of when the power is 1
+        powers = [number] #Here we make the powers for the square and multiply algorithm
+        for i in range(1, max(exponents)+1):
+            powers.append((powers[i-1]**2)%field_size)
+        r = 1
+        for j,val in enumerate(powers): #Here we just multiply through the values
+            if j in exponents:
+                r = (r*val)%field_size
+        return Element(self, r%self.cardinality) #one last call to modulus to guarantee, then return
 
     def square_root(self, number):
         if not self.is_square(number):
@@ -23,47 +48,51 @@ class GeneralField():
             return number,number,number
         elif self.cardinality%4 == 3:
             r = self.exponent(2 * number, (self.cardinality - 5)//4)
-            return number,r,field_size-r
+            return Element(self, number),Element(self, r),Element(self, field_size-r)
         elif self.cardinality % 8 == 5:
             v = self.exponent(2 * number, (self.cardinality - 5)//8)
             i = (2 * number * v * v) % self.cardinality
             r = (number*v*(i - 1)) % self.cardinality
-            return number,r,self.cardinality-r
+            return Element(self, number),Element(self, r),Element(self, self.cardinality-r)
         elif self.cardinality % 8 == 1:
-            r = self.shanks(number)
-            return number,r,self.cardinality-r
+            r = self.shanks(number) #this needs replacing since having a separate shanks method sucks
+            return Element(self, number),Element(self, r),Element(self, self.cardinality-r)
         return None
 
 class Element():
     def __init__(self, field, data):
-        self.data = data
+        self.data = data%field.cardinality
         self.field = field
 
     def __add__(self, other):
         if self.field == other.field:
-            return Element(self.field, (self.data + other.data) % self.field.cardinality)
+            return self.field.add(self.data, other.data)
         return None
 
     def __sub__(self, other):
         if self.field == other.field:
-            return Element(self.field, (self.data - other.data) % self.field.cardinality)
+            return self.field.sub(self.data, other.data)
         return None
 
     def __mult__(self, other):
         if self.field == other.field:
-            return Element(self.field, (self.data * other.data) % self.field.cardinality)
+            return self.field.mult(self.data, other.data)
         return None
+        
+    __rmult__ = __mult__
+    
+    __radd__ = __add__
 
     def exp(self, other):
         if other == self.field.characteristic:
             return Element(self.field, 1)
         else:
-            return Element(self.field, self.field.exponent( self.data, other.data))
+            return self.field.exponent(self, other)
 
     def square_root(self):
         return self.field.square_root(self))
 
-
+#Implement also a __lt__, __gt__, and etc
 
 '''
 #The following method can be severely generalized by passing it a method as its final argument, this being the polynomial relation to be raised to the higher power of the prime. As written, we require that there be implemented an element class which the arithmetic is operating on
