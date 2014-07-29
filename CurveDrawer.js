be8c65878f0addb43e812b0f1839a89ab417e412
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-07-29 11:49:58 -0700

    Update CurveDrawer.js
    Still trying to make labels. Not quite working.

--- CurveDrawer.js
+++ CurveDrawer.js
@@ -1,27 +1,7 @@
 //the page has only one svg element so select it
 var svg = d3.select("svg");
  
- //here we create the x axis. this involves appending a line tag and then assigning it the right coordinates
-var axisx = svg.append("line")
-.attr({
-  x1 : 0,
-  y1 : 450, //this has to be generalized to scale with the size of svg display
-  x2 : 600,
-  y2 : 450,
-  stroke : 'grey'
-}
-     );
- 
- //here we do the same, but for the y axis
-var axisy = svg.append("line")
-.attr({
-  x1 : 75, //this similarly has to be generalized to scale with the size of svg display
-  y1 : 0,
-  x2 : 75,
-  y2 : 600,
-  stroke : 'grey'
-}
-      );
+
  
  //this is some example data for a curve, in the future we'll just have a request that passes a similar array of points
 var data = [
@@ -75,3 +55,44 @@ var circles = group.append("circle")
  
  //This is the data for the labels. I am as yet undecided on how to implement it
 var label_data = ['0', '1', '2', '3', '4']
+
+var xaxis_holder = svg.append("g")
+  .attr("class", "xaxis");
+ //here we create the x axis. this involves appending a line tag and then assigning it the right coordinates
+var axisx = xaxis_holder.append("line")
+.attr({
+  x1 : 0,
+  y1 : 450, //this has to be generalized to scale with the size of svg display
+  x2 : 600,
+  y2 : 450,
+  stroke : 'grey'
+}
+     );
+ 
+var yaxis_holder = svg.append("g")
+  .attr("class", "yaxis");
+
+ //here we do the same, but for the y axis
+var axisy = svg.append("line")
+.attr({
+  x1 : 75, //this similarly has to be generalized to scale with the size of svg display
+  y1 : 0,
+  x2 : 75,
+  y2 : 600,
+  stroke : 'grey'
+}
+      );
+
+var xlabels = xaxis_holder.selectAll("g")
+  .data(label_data)
+  .enter()
+  .append("g");
+
+//After this line is where things break down
+xlabels.append("text")
+.attr("x", function(d) {return d*50;})
+.attr("y", 620)
+.attr(text, function(d) {return d;})
+.attr("font-family", "sans-serif")
+.attr("font-size", "20px")
+.attr("fill", "grey");
