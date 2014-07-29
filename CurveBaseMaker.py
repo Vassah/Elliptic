5f078a9bc65b6a679c230d52b7e134aa648fcf6d
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-07-29 10:18:50 -0700

    Drafting Database Populator
    We use the with statement since connections can use context management. Then we run through the primes in Primes.txt. Then, we run through the curves over that field, popping in the pointset.

--- CurveBaseMaker.py
+++ CurveBaseMaker.py
@@ -1,1 +1,17 @@
-//Begin here
+#Begin here
+import psycopg2
+import EllipticCurve as ec
+
+database_info = "" #Eventually this string will hold the login info like "dbname=xyz user=abc"
+with psycopg2.connect(database_info) as connecticus:
+  with connecticus.cursor() as curtesy:
+    with open("./Primes.txt") as primes:
+      for i in primes:
+        GFi = ec.GeneralField(i, 1)
+        curtesy.execute("CREATE TABLE field" + i + " id curve PRIMARY KEY") #STUFF HERE TO MAKE FIELD TABLE
+        for j in range(0,i):
+          for k in range(0,i):
+            curvacious = ec.EllipticCurve(GFi, [j, k])
+            pointses = curvacious.pointset
+
+          
