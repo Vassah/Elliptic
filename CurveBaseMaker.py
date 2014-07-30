d9173e4258adb4ab3d0b71e7cbea37b9989c16d6
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-07-29 20:46:01 -0700

    Update CurveBaseMaker.py
    trying to add SQL commands

--- CurveBaseMaker.py
+++ CurveBaseMaker.py
@@ -1,10 +1,10 @@
 import psycopg2
 import EllipticCurve as ec
 
-database_info = "" #Eventually this string will hold the login info like "dbname=xyz user=abc"
+database_info = "dbname=slazaron_curves user=slazaron password=" #Eventually this string will hold the login info like "dbname=xyz user=abc"
 with psycopg2.connect(database_info) as connecticus:
   with connecticus.cursor() as curtesy:
-    with open("./Primes.txt") as primes:
+    with open("./home4/Elliptic/Primes.txt") as primes:
       for i in primes:
         GFi = ec.GeneralField(i, 1)
         curtesy.execute("CREATE TABLE field" + i + " id curve PRIMARY KEY") #STUFF HERE TO MAKE FIELD TABLE
@@ -12,3 +12,7 @@ with psycopg2.connect(database_info) as connecticus:
           for k in range(0,i):
             curvacious = ec.EllipticCurve(GFi, [j, k])
             pointses = curvacious.pointset
+            curtesy.execute("ALTER TABLE field" + i + " ADD " + ec.coefficients + " string")
+            curtesy.execute("")
+            
+#WE NEED TO THINK ABOUT DATABASE DESIGN
